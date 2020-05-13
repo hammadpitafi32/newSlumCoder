4080cbb3f131fb8e2907c038ac38731d3bf73932
hammad khan <wt.hammadkhan@gmail.com>
2020-05-13 23:56:45 +0500

    post error fixed

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -73,30 +73,30 @@ class PostsController extends AppBaseController
         $input['user_id']=auth()->user()->id;
         $input['slug']=str_replace(' ','-',strtolower($request->title));
         
-        $detail=$request->input('content');
+        // $detail=$request->input('content');
         
-        $dom = new \DomDocument();
+        // $dom = new \DomDocument();
 
-        $dom->loadHtml($detail, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
-        $images = $dom->getElementsByTagName('img');
+        // $dom->loadHtml($detail, LIBXML_HTML_NOIMPLIED | LIBXML_HTML_NODEFDTD);
+        // $images = $dom->getElementsByTagName('img');
 
-        foreach($images as $k => $img){
+        // foreach($images as $k => $img){
 
-        $data = $img->getAttribute('src');
-        list($type, $data) = explode(';', $data);
-        list(, $data)      = explode(',', $data);
-        $data = base64_decode($data);
-        $image_name= '/uploads/' . time().$k.'.png';
-        $path = public_path() . $image_name;
-        file_put_contents($path, $data);
-        $img->removeAttribute('src');
-        $img->setAttribute('src', $image_name);
+        // $data = $img->getAttribute('src');
+        // list($type, $data) = explode(';', $data);
+        // list(, $data)      = explode(',', $data);
+        // $data = base64_decode($data);
+        // $image_name= '/uploads/' . time().$k.'.png';
+        // $path = public_path() . $image_name;
+        // file_put_contents($path, $data);
+        // $img->removeAttribute('src');
+        // $img->setAttribute('src', $image_name);
 
-        }
+        // }
 
-        $detail = $dom->saveHTML();
+        // $detail = $dom->saveHTML();
 
-        $input['content']= $detail;
+        // $input['content']= $detail;
 
         $posts = $this->postsRepository->create($input);
 
