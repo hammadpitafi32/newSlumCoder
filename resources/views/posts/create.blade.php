db2bfb2cb1147869e7dbded99b194f60a97b44ca
hammad khan <wt.hammadkhan@gmail.com>
2020-07-19 12:25:46 +0500

    fix editor

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -31,11 +31,17 @@
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
 <!-- <script src="https://cdn.jsdelivr.net/npm/summernote@0.8.15/dist/summernote.min.js"></script> -->
 <script src="https://cdn.tiny.cloud/1/uy0zgera9l80yghcdlg3qmcg9pog8pp6sx3x9sysk1gos51k/tinymce/5/tinymce.min.js" referrerpolicy="origin"></script>
+
 <script>
-      tinymce.init({
-        selector: '#summernote'
-      });
-</script>
+    tinymce.init({
+      selector: 'textarea',
+      plugins: 'a11ychecker advcode casechange formatpainter linkchecker autolink lists checklist media mediaembed pageembed permanentpen powerpaste table advtable tinycomments tinymcespellchecker image',
+      toolbar: 'a11ycheck addcomment showcomments casechange checklist code formatpainter pageembed permanentpen table | image link media',
+      toolbar_mode: 'floating',
+      tinycomments_mode: 'embedded',
+      tinycomments_author: 'Hammad',
+    });
+  </script>
 
 <!-- <script type="text/javascript">
 
